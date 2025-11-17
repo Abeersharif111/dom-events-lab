10fbaf1f44d62f3283f884d9bf83473086eca751
Abeersharif111 <ab.a.r.a.sharif1@gmail.com>
2025-11-18 00:27:03 +0300

    edited but still not complete

--- app.js
+++ app.js
@@ -3,39 +3,79 @@
 /*-------------------------------- Variables --------------------------------*/
 let num1=null;
 let num2=null;
-let operator =null;
+
+let button = null;
+let value = null;
+let result = null;
 /*------------------------ Cached Element References ------------------------*/
 const display = document.querySelector('.display')
-const buttons = document.querySelector('.button')
+const buttons = document.querySelectorAll('.button')
+console.log(buttons)
+const calculator = document.querySelector('#calculator');
+const row = document.querySelector('.row')
+
 /*----------------------------- Event Listeners -----------------------------*/
-for ( i=0;i<buttons.length; i++)
-{ buttons[i].addvEentLister("click",console.log('clicked')) }
-//buttons[2].addEventListener('click', console.log('3 clicked'))
+
+buttons.forEach ( button => {
+  button.addEventListener('click', (buttonClickedHandler) )} ) // ..working
+  //=> {
+    // This log is for testing purposes to verify we're getting the correct value
+   // console.log(event.target.innerText); ....working
+    // Future logic to capture the button's value would go here
+       
+  
+
+
+
+
 /*-------------------------------- Functions --------------------------------*/
-//function buttonClicked( b ) 
-//{
-//let value = b.target.textContent ; // give the content of the clicked button to the value
-//console.log(value.textContenet)
-//}
+function buttonClickedHandler( event ) 
+{
+let value =event.target.textContent ; // give the content of the clicked button to the value ...not working
+console.log(event.target.textContent)  // to check the function in consle ....working
 
-/* if (value >='0' && value<='9')
-  { holdNumber(value)
-    return
-  }
-*/
 
-/* if ( value ==='C')  // to clear the display content
+
+ //  if (event.target.classList.contains('number')) // if number is pressed ... not working
+ //   { holdNumber(value) 
+ //      return }
+
+
+ if (value >='0' && value<='9')   // if a number is pressed    ..... working
+  { holdNumber(value) }           // call function holdNumber
+
+
+ // if (event.target.innerText === '*')  // if operator is pressed ....not working
+ //  {
+    // Do something with this operator
+ // }
+
+if ( value === '+')
+    { 
+      //operator = value
+      //value = null 
+      operator(value)
+     }
+
+     
+
+ if ( value ==='C')  // to clear the display content
    { clearAll() 
-      return 
+      
    }
 
 if ( value ==='=') // to show the result
-{ 
-    result ()
-    return
+   { 
+      calculate()
+    console.log(display)
+   //result ()
+    //return
+   }
+
 }
-*/
-/*function holdNumber(value) 
+
+
+function holdNumber(value) 
 {
     if (num1 === null) // if no numbers clicked yet
        { num1= value }   // give the vlaue to num1
@@ -55,10 +95,29 @@ if ( value ==='=') // to show the result
  }
 }
 
-function clearAll() 
+
+function operator() 
+       {
+         if (num1===null )    // if still no numbers pressed
+         return;
+
+         if ( num2 !== null)
+            {calculate()
+            }
+       }
+
+function calculate( )
+     { 
+      result = Number(num1.value) + Number(num2.value)
+      display.textContent = result
+     }
+
+
+
+ function clearAll() 
 {
     num1= null
     num2= null
-    operatopr= null
+  //  operatopr= null
     display.textContent = null
-} */
+} 
